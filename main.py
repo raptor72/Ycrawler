e320d0e29f8755df78a4dabc7cad79e7cbf785a7
raptor72 <mctaras51@yandex.ru>
2020-09-02 21:38:07 +0400

    Add strongly sync begginning steps

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
+import os
+import logging
 import asyncio
 import aiohttp
+import requests
+from bs4 import BeautifulSoup
 
 url = 'https://news.ycombinator.com'
+
+def get_file(url):
+    r = requests.get(url, allow_redirects=True)
+    return r.content
+
+def get_news_names(html):
+    names = []
+    soup = BeautifulSoup(html, 'lxml')
+    blocks = soup.find_all('td', class_='title')
+    # print(blocks)
+    # logging.INFO(f'{blocks}')
+    for block in blocks:
+        try:
+            link = block.find('a', class_='storylink').get('href')
+            name = block.find('a', class_='storylink').get_text()
+        except:
+            continue
+        if link:
+            print(link)
+            print(name)
+        # print(block)
+        # print()
+        # names.append(name)
+    # return names
+
+
+def main():
+    html = get_file(url)
+    return get_news_names(html)
+
+# def write_image(data):
+#     dirname = None
+#     filename = 'file-{}.jpeg'.format(int(time() * 1000))
+#     with open(filename, 'wb') as file:
+#         file.write(data)
+
+
+# def get_current_news():
+
+# async def fetch_content(url, session):
+#     async with session.get(url, allow_redirects=True) as response:
+#         data = await response.read()
+
+
+
+if __name__ == "__main__":
+    print(main())
 
